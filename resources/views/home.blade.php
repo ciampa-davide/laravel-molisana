eaf2f7b4bc1e705fe90b78f2b73f944f56fec86f
Davide <ciampad06@gmail.com>
2021-02-03 17:31:18 +0100

    inizio progetto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="{{ asset ('images/marchio-sito-test.png') }}" alt="">
+        <ul class="list-inline">
+            <li class="active">
+                <a href="#">HOME</a>
+            </li>
+            <li>
+                <a href="#">PRODOTTI</a>
+            </li>
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+        </ul>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
